629882fdadc74d27aa111fbd130eb0be029e7c17
chinmayamr <91525938+chinmayamr@users.noreply.github.com>
2023-12-13 22:54:46 +0530

    Update iit.py

--- iit.py
+++ iit.py
@@ -1,1 +1,13 @@
+import streamlit as st
+
+def find_largest(num1, num2, num3):
+  largest = max(num1, num2, num3)
+  return largest
+st.title("Find the Largest Among 3 Numbers")
+num1 = st.number_input("Enter Number 1:")
+num2 = st.number_input("Enter Number 2:")
+num3 = st.number_input("Enter Number 3:")
+if st.button("Find Largest"):
+  largest_number = find_largest(num1, num2, num3)
+  st.write(f"The largest number is: {largest_number}")
 
